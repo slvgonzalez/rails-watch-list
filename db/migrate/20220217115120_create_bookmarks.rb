3dbc167a8d5005dc784abf10e1ec33d9244cc083
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-17 12:32:41 +0000

    db created and migrated attributes and references

--- db/migrate/20220217115120_create_bookmarks.rb
+++ db/migrate/20220217115120_create_bookmarks.rb
@@ -2,9 +2,8 @@ class CreateBookmarks < ActiveRecord::Migration[6.1]
   def change
     create_table :bookmarks do |t|
       t.string :comment
-      t.references :movie_id, foreign_key: true
-      t.references :list_id, foreign_key: true
-
+      t.references :movie, foreign_key: true
+      t.references :list, foreign_key: true
       t.timestamps
     end
   end
